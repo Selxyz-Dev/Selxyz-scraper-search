9ed39d001b4622de42bb9bab92563d6ae153297e
Selxyz <selxyzdev@gmail.com>
2024-11-29 16:45:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+import { anichin, anichinDetail } from './Donghua/anichin.js';
+import { donghuaworld, donghuaworldDetail } from './Donghua/donghuaworld.js'
+
+export { anichin, anichinDetail, donghuaworld, donghuaworldDetail }
